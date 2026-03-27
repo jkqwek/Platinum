85210815a30f0014e9ec5983e54a52cab3f52f00
Sophia Voronina <sofiavoronina1@yandex.ru>
2026-03-27 22:09:35 +0300

    add screens

---
+++ lib/screens/library_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LibraryScreen extends StatelessWidget {
+  const LibraryScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Library'),
+      ),
+      body: const Center(
+        child: Text('Здесь будет библиотека игр'),
+      ),
+    );
+  }
+}
